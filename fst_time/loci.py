ab14bc67d2fecda0fe044a4a3f3a2649ac3c79a6
ohad peled <121860687+ohad-peled@users.noreply.github.com>
2026-08-27 13:03:56 +0300

    Update loci.py

--- fst_time/loci.py
+++ fst_time/loci.py
@@ -18,17 +18,12 @@ its name of that time.
 FOCAL_LOCI = [
 	{'label': 'SLC24A5', 'genes': ['SLC24A5'], 'phenotype': 'pigmentation', 'trend': 'high', 'time_bp': 8500},
 	{'label': 'SLC45A2', 'genes': ['SLC45A2'], 'phenotype': 'pigmentation', 'trend': 'high', 'time_bp': 4500},
-	{'label': 'OCA2', 'genes': ['OCA2'], 'phenotype': 'pigmentation', 'trend': 'high', 'time_bp': 8500},
+	{'label': 'HERC2/OCA2', 'genes': ['OCA2', 'HERC2'], 'phenotype': 'pigmentation', 'trend': 'high', 'time_bp': 8500},
 	{'label': 'EDAR', 'genes': ['EDAR'], 'phenotype': 'ectodermal morphology', 'trend': 'high', 'time_bp': None},
-	{'label': 'EPAS1', 'genes': ['EPAS1'], 'phenotype': 'high altitude hypoxia', 'trend': 'high', 'time_bp': None},
-	{'label': 'LCT', 'genes': ['LCT'], 'phenotype': 'lactase persistence', 'trend': 'low', 'time_bp': 4500},
+	{'label': 'LCT/MCM6 ', 'genes': ['LCT', 'MCM6'], 'phenotype': 'lactase persistence', 'trend': 'low', 'time_bp': 4500},
 	{'label': 'FADS1_FADS2', 'genes': ['FADS1', 'FADS2'], 'phenotype': 'lipid metabolism', 'trend': 'low', 'time_bp': 8500},
-	{'label': 'ENSA', 'genes': ['ENSA'], 'phenotype': 'insulin signalling', 'trend': 'low', 'time_bp': 8500},
 	{'label': 'TLR6_TLR1_TLR10', 'genes': ['TLR6', 'TLR1', 'TLR10'], 'phenotype': 'innate immunity', 'trend': 'low', 'time_bp': 8500},
 	{'label': 'MARK3', 'genes': ['MARK3'], 'phenotype': 'infectious disease immunity', 'trend': 'low', 'time_bp': 4500},
 	{'label': 'IL23R', 'genes': ['IL23R'], 'phenotype': 'immune signalling', 'trend': 'low', 'time_bp': 4500},
-	{'label': 'TYK2', 'genes': ['TYK2'], 'phenotype': 'immune signalling', 'trend': 'low', 'time_bp': 4500},
-	{'label': 'IL1RL1', 'genes': ['IL1RL1'], 'phenotype': 'immune signalling', 'trend': 'low', 'time_bp': 3150},
-	{'label': 'ERAP2', 'genes': ['ERAP2'], 'phenotype': 'antigen processing, plague', 'trend': 'low', 'time_bp': 700},
-	{'label': 'ABO', 'genes': ['ABO'], 'phenotype': 'blood group, pathogen binding', 'trend': 'low', 'time_bp': None},
+	{'label': 'IL1RL1', 'genes': ['IL1RL1'], 'phenotype': 'immune signalling', 'trend': 'low', 'time_bp': 3150}
 ]
